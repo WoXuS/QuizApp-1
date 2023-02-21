6784a63a5a12dbee6d5f23fa946d64e5cc51e2d2
Kamil Wozniak <kamilwozniak158@gmail.com>
2023-02-22 00:25:33 +0100

    changed api url

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from "@angular/core";
   providedIn: 'root'
 })
 export class ApiService {
-  protected readonly apiUrl: string = 'https://localhost:4200/api/';
+  protected readonly apiUrl: string = 'https://taiquizapp.azurewebsites.net/api/';
 
   constructor(
     protected http: HttpClient,
